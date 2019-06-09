cf06d8d42d5d885f3827f5a5a60595b94117d544
FrankWu <M10623004@gemail.yuntech.edu.tw>
2019-06-10 04:46:03 +0800

    timeline 進行中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,42 +2,56 @@
   <div class="hello">
     <div>
     </div>
-    <h1>{{ 123 }}</h1>
-      <v-container grid-list-md text-xs-center fluid>
+    <v-container grid-list-md text-xs-center fluid>
       <v-layout row wrap>
-          <v-flex d-flex xs12 sm6 md3>
-          <v-layout row wrap>
-            <v-flex d-flex  md12>
-              <v-card color="indigo" dark>
-                 <v-container>
-                   <v-avatar color="#0D9298" size="50px">
-                      <span class="white--text headline">123</span>
+        <v-flex d-flex xs12 sm6 md3>
+          <v-layout row wrap align-center="">
+            <v-flex d-flex md12 id="hello">
+              <v-card>
+                <v-container>
+                  <v-avatar size="200px" color="green">
+                    <img
+                      src="https://scontent-tpe1-1.cdninstagram.com/vp/f576d2a9217ef310f6e65dbd4aa6eb71/5D8B40A1/t51.2885-15/e35/47583983_359007271474690_4349700514170901752_n.jpg?_nc_ht=scontent-tpe1-1.cdninstagram.com"
+                      alt="headshot">
                   </v-avatar>
-                  <H1>Android Development</H1>
-                 </v-container>
-              </v-card>
-            </v-flex>
-            <v-flex d-flex  md12>
-              <v-card color="indigo" dark>
-                <v-card-text>123</v-card-text>
+                  <H1>吳承剛</H1>
+                  <H2>Android Development</H2>
+                </v-container>
               </v-card>
             </v-flex>
           </v-layout>
         </v-flex>
-         <v-flex d-flex xs12 sm6 md9>
+        <v-flex d-flex xs12 sm6 md9>
           <v-layout row wrap>
             <v-flex d-flex md12>
-              <v-card color="indigo" dark>
-              </v-card>
-            </v-flex>
-            <v-flex d-flex md12>
-              <v-card color="indigo" dark>
+              <v-card>
+                <v-timeline align-center dense>
+                  <v-timeline-item v-for="(year, i) in years" :key="i" :color="year.color" small>
+                    <v-layout row wrap align-center="">
+                    <v-flex d-flex xs12 sm6 md4>
+                      <template>
+                        <span :class="`headline font-weight-bold ${year.color}--text`" v-text="year.year"></span>
+                      </template>
+                    </v-flex>
+                    <v-flex d-flex xs12 sm6 md8>
+                      <div class="py-3">
+                      <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">Lorem ipsum</h2>
+                      <div>
+                        Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed
+                        euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando
+                        voluptatibus, vix an salutandi sententiae.
+                      </div>
+                    </div>
+                    </v-flex>
+                    </v-layout>
+                  </v-timeline-item>
+                </v-timeline>
               </v-card>
             </v-flex>
           </v-layout>
-         </v-flex>
-       </v-layout>
-      </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -49,7 +63,27 @@ export default {
   data () {
     return {
       active: null,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      years: [
+      {
+        color: 'cyan',
+        year: '2017-2019',
+        school:'國立雲林科技大學',
+        department:'資訊管理所'
+      },
+      {
+        color: 'green',
+        year: '2015-2017',
+        school:'國立雲林科技大學',
+        department:'資訊管理系'
+      },
+      {
+        color: 'pink',
+        year: '2010-2015',
+        school:'國立台中科技大學',
+        department:'銀行保險科'
+      }
+    ]
     }
   },
   methods: {
@@ -63,21 +97,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.hello{
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  #hello {
+    margin: 30px;
+  }
+
+  H1 {
+    font-size: 30px;
+    font-weight: bold;
+    font-family: 'Microsoft JhengHei';
+    margin: 10px 10px;
+  }
+
+  H2 {
+    font-size: 16px;
+    margin: 15px 10px;
+  }
+
+  img {
+    padding: 1px;
+  }
+
 </style>
